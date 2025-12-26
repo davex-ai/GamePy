117351c3079fa7f6535a20fc5397515ed9846ced
davex-ai <omotoyedave4@gmail.com>
2025-12-26 03:24:22 +0100

    safe guard

--- number_guess.py
+++ number_guess.py
@@ -1,22 +1,35 @@
 import random
-
-def guess(x):
-    return random.randint(1, x)
-
-answer = guess(100)
-tries = 0
 print("=" * 40)
 print("Welcome to the Number Guessing Game🎉🎉")
 print("=" * 40)
-choice = int(input("Guess the Number between 1 - 100: "))
-while choice != answer:
-    if choice > answer:
-        choice = int(input("TOO BIG\nTry Again: "))
-        tries +=1
-    elif choice < answer:
-       choice = int(input("TO SMALL\nTry Again: "))
-       tries += 1
-    else:
-        print("Invalid Input")
-if choice == answer:
+try:
+    length = int(input("Set your length: "))
+
+    def guess(x):
+        return random.randint(1, x)
+
+    answer = guess(length)
+    tries = 0
+
+    choice = 0
+    try:
+        choice = int(input(f"Guess the Number between 1 - {length}: "))
+    except ValueError:
+        print("Enter an integer value")
+
+    while choice != answer:
+        if choice > length:
+            choice = int(input(f"{choice} is out of range \nTry Again, 1 - {length}: "))
+            tries +=1
+        elif choice > answer:
+            choice = int(input("TOO BIG\nTry Again: "))
+            tries +=1
+        elif choice < answer:
+           choice = int(input("TO SMALL\nTry Again: "))
+           tries += 1
+
     print(f"Correct!! It took you {tries} tries")
+except KeyboardInterrupt:
+    print("\nPrograms was Interrupted")
+except ValueError:
+    print("Enter an integer value")
