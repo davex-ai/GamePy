d657d72440277e816c555e911cd4d8774f0e0d18
davex-ai <omotoyedave4@gmail.com>
2025-12-27 00:00:38 +0100

    .....

--- tick_toe.py
+++ tick_toe.py
@@ -48,8 +48,35 @@ class TickTacToe:
 
     def available_moves(self):
         return [i for i, spot in enumerate(self.board) if spot == " "]
-        moves = []
-        for (index, spot) in enumerate(self.board):
-            if spot == ' ':
-                moves.append(spot)
+        # moves = []
+        # for (index, spot) in enumerate(self.board):
+        #     if spot == ' ':
+        #         moves.append(spot)
 
+    def empty_squares(self):
+        return " " in self.board
+
+    def num_empty_squares(self):
+        self.board.count(" ")
+
+    def make_move(self, square, letter):
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            return True
+        return False
+
+def play(game, x_player, o_player, print_game= True):
+    if print_game:
+        game.print_board_num()
+
+    letter = 'x'
+    while game.empty_squares():
+        if letter == 'o':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f"makes a move to square {square}")
+                game.print_board()
+                print('')
