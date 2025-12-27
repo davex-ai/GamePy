a2ff9e713208420961d3f3f6ba95ef66c15696c4
davex-ai <omotoyedave4@gmail.com>
2025-12-27 13:05:17 +0100

    ....

--- tick_toe.py
+++ tick_toe.py
@@ -113,8 +113,47 @@ def play(game, x_player, o_player, print_game= True):
 
     if print_game:
         print("Its a tie")
+
+class GeniusComputer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)["position"]
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'o' if player == 'x' else 'x'
+        if state.current_winner == other_player:
+            return {"position": None, "square": 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+
+        elif not state.empty_squares():
+            return  {"position": None, "score": 0}
+        if player == max_player:
+            best = {"position": None, "score": -math.inf}
+        else:
+            best = {"position": None, "score": math.inf}
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move
+            if player == max_player:
+                if sim_score["score"] > best["score"]:
+                    best = sim_score
+            else:
+                if sim_score["score"] <best["score"]:
+                    best = sim_score
+
+
+        return best
 if __name__  == '__main__':
     x_player = HumanPLayer('x')
-    o_player = ComputerPLayer('o')
+    o_player = GeniusComputer('o')
     t = TickTacToe()
     play(t,x_player, o_player, True)
