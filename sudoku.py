ba10c13528e5cc5bd553c1d43ecec0cae70c5342
davex-ai <omotoyedave4@gmail.com>
2025-12-28 17:33:23 +0100

    sudoku

--- sudoku.py
+++ sudoku.py
@@ -5,5 +5,17 @@ def find_next_empty(puzzle):
                 return puzzle[r][c]
     return None, None
 
+def is_valid(puzzle, guess, row, col):
+    row_val = puzzle[row]
+    if guess in row_val:
+        return False
+    col_val = [puzzle[i][col] for i in range(9)]
+    if guess in col_val:
+        return False
+
 def solve_sudoku(puzzle):
     row, col = find_next_empty(puzzle)
+    if row is None:
+        return  True
+    for guess in range(1, 10):
+        if is_valid(puzzle, guess, row, col):
