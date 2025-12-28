901a87e01bd0392fb50095e86a3810ef8ff1124a
davex-ai <omotoyedave4@gmail.com>
2025-12-28 16:24:11 +0100

    minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -73,6 +73,34 @@ class Board:
                     visible_board[row][col] = str(self.board[row][col])
                 else:
                     visible_board[row][col] = ' '
+        string_rep = ''
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(len(max(columns, key=len)))
+
+        indices = [i for  i in range(self.dim_size)]
+        indices_row = '  '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += ' '.join(cells)
+        indices_row += ' \n'
+
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+        string_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*string_len + '\n' + string_rep + '-'*string_len
+        return string_rep
+
 
 
 
